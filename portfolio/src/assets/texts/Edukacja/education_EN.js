0743c284c056f6c5029c3457d64ddffeb2b22b11
Maciej Uberna <maciej.uberna@gmail.com>
2021-11-04 18:09:39 +0100

    Text desc added for job experience and education.

---
+++ portfolio/src/assets/texts/Edukacja/education_EN.js
@@ -0,0 +1,12 @@
+export default {
+    "Education":[
+        {
+            "start-date":"10.2000",
+            "end-date":"12.2008",
+            "school-name":"PJATK",
+            "education-level":"master of science",
+            "subject":"IT",
+            "specialization":"3D Animation, voice recognition (multimedia)."
+        }
+    ]
+}
---
+++ portfolio/src/assets/texts/Edukacja/edukacja_PL.js
@@ -0,0 +1,12 @@
+export default {
+    "Education":[
+        {
+            "start-date":"10.2000",
+            "end-date":"12.2008",
+            "school-name":"PJATK",
+            "education-level":"magister inżynier",
+            "subject":"informatyka",
+            "specialization":"Animacja 3D, rozpoznawanie mowy"
+        }
+    ]
+}
