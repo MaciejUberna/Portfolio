f7ba3d7dad00547fac7e71c18339c47048050119
Maciej Uberna <maciej.uberna@gmail.com>
2021-11-16 13:18:23 +0100

    Added swicher so NavigationItems can switch via props to buttons or backdrop.

--- portfolio/src/coponents-stateLess/Navigation/NavigationItems/NavigationItems.js
+++ portfolio/src/coponents-stateLess/Navigation/NavigationItems/NavigationItems.js
@@ -1,16 +1,32 @@
-import React from 'react';
- 
+import React, { useState } from 'react';
+
 import classes from './NavigationItems.module.scss'
 import NavigationItem from './NavigationItem/NavigationItem';
+import LanguageDropdown from './LanguageDropdown/LanguageDropdown';
+import LanguageButtons from './LanguageButtons/LanguageButtons';
+
+// ñ
  
-const navigationItems = (props) => {
+const NavigationItems = (props) => {
+
+    let languageSwitcher = null;
+
+    if(props.languageSwitcherType === 'dropdown') {
+        languageSwitcher = (<LanguageDropdown/>);
+        //console.log('languageSwitcher: ',languageSwitcher);
+     } else if(props.languageSwitcherType === 'buttons') {
+         languageSwitcher = (<LanguageButtons/>);
+         //console.log('languageSwitcher: ',languageSwitcher);
+     }
+
     return (
         <ul className={classes.NavigationItems}>
             <NavigationItem link="/edukacja" >Edukacja</NavigationItem>
             <NavigationItem link="/doswiadczenie" >Doświadczenie zawodowe</NavigationItem>
             <NavigationItem link="/projekty" >Projekty</NavigationItem>
+            {languageSwitcher}
         </ul>
     );
-}
+};
  
-export default navigationItems;
+export default NavigationItems;
