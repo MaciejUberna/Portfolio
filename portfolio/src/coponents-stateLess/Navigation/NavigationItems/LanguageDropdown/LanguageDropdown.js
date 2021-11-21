b036d9ef4a7d5136358896b08f0a0413b13fc86f
Maciej Uberna <maciej.uberna@gmail.com>
2021-11-21 22:18:51 +0100

    First ever working correctlly implementation of LanguageDropdown done on react hooks.

--- portfolio/src/coponents-stateLess/Navigation/NavigationItems/LanguageDropdown/LanguageDropdown.js
+++ portfolio/src/coponents-stateLess/Navigation/NavigationItems/LanguageDropdown/LanguageDropdown.js
@@ -1,98 +1,79 @@
-import React, {useContext} from 'react';
+import React, {useContext, useRef} from 'react';
 import LanguageContext from '../../../../mainStates/language/language-context';
+import { useDetectOutsideClick } from "../../../../customHooks/clickListenerHook";
 
 import cls from './LanguageDropdown.module.scss';
-import Dropdown from 'react-bootstrap/Dropdown';
-// import DropdownButton from 'react-bootstrap/DropdownButton';
-// import ButtonGroup from "react-bootstrap/ButtonGroup";
 import {ReactComponent as PL} from '../../../../assets/images/flags/pl.svg';
 import {ReactComponent as EN} from '../../../../assets/images/flags/gb.svg';
 import {ReactComponent as ES} from '../../../../assets/images/flags/es.svg';
 import {ReactComponent as Unknown} from '../../../../assets/images/flags/xx.svg';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 const LanguageDropdown = (props) => {
     const languageContext = useContext(LanguageContext);
     let welcomeMessage = null;
 
     if (languageContext.language === 'pl'){
-        welcomeMessage = ( <div className={cls.DrpDwn__Item}>
-            <PL className={cls.DrpDwn__Svg}/>&nbsp;<span>Język</span>
-            <span className={cls.DrpDwn__ArrowDown}>&nbsp;&#10225;</span>
+        welcomeMessage = ( <div className={cls.menu_svg_text}>
+            <PL className={cls.menu_svg}/>&nbsp;<span>Język</span>
+            <span className={cls.menu_arrow}>&nbsp;&#10225;</span>
         </div>);
     } else if(languageContext.language === 'en'){
-        welcomeMessage = ( <div className={cls.DrpDwn__Item}>
-            <EN className={cls.DrpDwn__Svg}/>&nbsp;<span>Language</span>
-            <span className={cls.DrpDwn__ArrowDown}>&nbsp;&#10225;</span>
+        welcomeMessage = ( <div className={cls.menu_svg_text}>
+            <EN className={cls.menu_svg}/>&nbsp;<span>Language</span>
+            <span className={cls.menu_arrow}>&nbsp;&#10225;</span>
         </div>);
     } else if(languageContext.language === 'es'){
-        welcomeMessage = ( <div className={cls.DrpDwn__Item}>
-            <ES className={cls.DrpDwn__Svg}/>&nbsp;<span>Idioma</span>
-            <span className={cls.DrpDwn__ArrowDown}>&nbsp;&#10225;</span>
+        welcomeMessage = ( <div className={cls.menu_svg_text}>
+            <ES className={cls.menu_svg}/>&nbsp;<span>Idioma</span>
+            <span className={cls.menu_arrow}>&nbsp;&#10225;</span>
         </div>);
     } else {
-        welcomeMessage = ( <div className={cls.DrpDwn__Item}>
-            <Unknown className={cls.DrpDwn__Svg}/>
+        welcomeMessage = ( <div className={cls.menu_svg_text}>
+            <Unknown className={cls.menu_svg}/>
             &nbsp;???&nbsp;
-            <span className={cls.DrpDwn__ArrowDown}>
+            <span className={cls.menu_arrow}>
                 &#10225;
             </span>
         </div>);
         console.log(languageContext.language);
     }
 
-    const handleSelect=(e)=>{
-        console.log(e);
-    };
+    const itemClick = (e) => {
+      console.log(e);
+    }
 
     const dropdownRef = useRef(null);
-    const [isActive, setIsActive] = useState(false);
+    const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
     const onClick = () => setIsActive(!isActive);
+    let dropdownMenuClass = isActive ? 
+      `${cls.menu} ${cls.menu_container_active}` 
+      : 
+      `${cls.menu} ${cls.menu_container_inactive}`;
 
     return (
-        // <DropdownButton 
-        //     onSelect={handleSelect}
-        //     className={cls.DrpDwn}
-        //     title={welcomeMessage}
-        //     variant='secondary'
-        //     as={ButtonGroup}
-        // >
-        // {/* <Dropdown.Header className={cls.DrpDwn__Menu}> -------------- </Dropdown.Header> */}
-        // {/* <Dropdown.Toggle variant="secondary" className={cls.Dropdown} bsSize="small">
-        //     {welcomeMessage}
-        // </Dropdown.Toggle> */}
-        //     <Dropdown.Menu  className={cls.DrpDwn__Menu}>
-        //         <Dropdown.Item eventKey="pl">
-        //             <PL className={cls.DrpDwn__Svg}/>
-        //             &nbsp;
-        //             Polski
-        //             </Dropdown.Item>
-        //         <Dropdown.Item eventKey="en">
-        //             <EN className={cls.DrpDwn__Svg}/>
-        //             &nbsp;
-        //             English
-        //         </Dropdown.Item>
-        //         <Dropdown.Item eventKey="es">
-        //             <ES className={cls.DrpDwn__Svg}/>
-        //             &nbsp;
-        //             Español
-        //         </Dropdown.Item>
-        //     </Dropdown.Menu>
-        // </DropdownButton>
-
-        <div className="menu-container">
-        <button onClick={onClick} className="menu-trigger">
-          {welcomeMessage}
-        </button>
-        <nav ref={dropdownRef} className={`menu ${isActive ? 'active' : 'inactive'}`}>
-          <ul>
-            <li><a href="/messages">Messages</a></li>
-            <li><a href="/trips">Trips</a></li>
-            <li><a href="/saved">Saved</a></li>
-          </ul>
-        </nav>
-      </div>
-
+        <div className={cls.menu}>
+          <div className={cls.menu_container}>
+            <button onClick={onClick} className={cls.menu_trigger}>
+              {welcomeMessage}
+            </button>
+            <nav
+              ref={dropdownRef}
+              className={dropdownMenuClass}
+            >
+              <ul>
+                <li onClick={itemClick.bind(this,'pl')}>
+                  <PL className={cls.menu_svg}/> Polski
+                </li>
+                <li onClick={itemClick.bind(this,'en')}>
+                  <EN className={cls.menu_svg}/> English
+                </li>
+                <li onClick={itemClick.bind(this,'es')}>
+                  <ES className={cls.menu_svg}/> Español
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
     );
 };
 
