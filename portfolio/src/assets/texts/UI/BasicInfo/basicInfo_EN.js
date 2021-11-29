a67c1475ba03886c438dce7206432791c40f8649
Maciej Uberna <maciej.uberna@gmail.com>
2021-11-29 13:43:38 +0100

    Not using these basicInfo file adding these just in case something goes wrong, or my coding plan will change.

---
+++ portfolio/src/assets/texts/UI/BasicInfo/basicInfo_EN.js
@@ -0,0 +1,8 @@
+export default {
+    "Name": "Maciek",
+    "Surname": "Uberna",
+    "E-mail": "maciej.uberna@gmail.com",
+    "Phone": "690 609 088",
+    "Date of birth": "01.08.1981",
+    "Accomodation": "Józefosław near Warsaw"
+};
---
+++ portfolio/src/assets/texts/UI/BasicInfo/basicInfo_ES.js
@@ -0,0 +1,8 @@
+export default {
+    "Nombre" : "Maciej",
+    "Apellido" : "Uberna",
+    "E-mail" : "maciej.uberna@gmail.com",
+    "Teléfono" : "(+48) 690 609 088",
+    "Fecha de nacimiento": "01.08.1981",
+    "Alojamiento" : "Józefosław cerca de Varsovia"
+}
