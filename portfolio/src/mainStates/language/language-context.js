77e6c011f8a34322fe1cba702180674c97218a49
Maciej Uberna <maciej.uberna@gmail.com>
2021-11-29 13:01:18 +0100

    language-context.js corrected no significant changes.

--- portfolio/src/mainStates/language/language-context.js
+++ portfolio/src/mainStates/language/language-context.js
@@ -1,5 +1,8 @@
 import React from "react";
+import {mainInfo,mainMenu} from '../../assets/texts/UI/ui';
 
-const LanguageContext = React.createContext({language: 'pl'});
+const LanguageContext = React.createContext({
+    language: 'pl'
+});
 
 export default LanguageContext;
