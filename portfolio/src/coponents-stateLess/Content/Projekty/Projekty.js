52d2fca8410fd9da33b5d8236c0d1ae64882a3e7
Maciej Uberna <maciej.uberna@gmail.com>
2021-12-02 19:44:05 +0100

    Done Projecty.js for all resolutions.

--- portfolio/src/coponents-stateLess/Content/Projekty/Projekty.js
+++ portfolio/src/coponents-stateLess/Content/Projekty/Projekty.js
@@ -1,8 +1,78 @@
-import React from 'react';
+import React, {useContext, useState, useEffect} from 'react';
+import cls from  './Projekty.module.scss';
+import Tryby from '../../../assets/images/Projects/3zebatki.svg';
+import languageContext from '../../../mainStates/language/language-context';
 
-const Projekty = (props) => {
+const Projekty = () => {
+    const lctx = useContext(languageContext);
+    const [lang,setLang] = useState(lctx.x.language);
+
+    useEffect(() => {
+        setLang(lctx.x.language);
+    },[lctx.x.language]);
+
+    const openInNewTab = (url) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
+        if (newWindow) newWindow.opener = null;
+    }
+
+    const onClickUrl = (url) => {
+        if(url!=='-------')
+            return () => openInNewTab(url);
+    };
+
+    const prjArr = lctx.x.projects[lang];
+    const displayArr = [];
+    for(let i=0;i<prjArr.length;i++){
+        let ctr=0;
+        const displayElement = [];
+        for(let key in prjArr[i]) {
+            if(ctr===4) {
+                displayElement.push(
+                    <li  
+                        key={i+key}
+                        onClick={onClickUrl(prjArr[i][key])}>
+                        <span className={cls.Main_Container_Link}> {key} :</span> 
+                        <span className={cls.Main_Container_Link}> {prjArr[i][key]} </span>
+                    </li>
+                );
+            } else {
+                displayElement.push(
+                    <li key={i+key}>
+                        <span className={cls.Main_Container_Name}> {key} :</span> 
+                        <span className={cls.Main_Container_Desc}> {prjArr[i][key]} </span>
+                    </li>
+                );
+            };
+            ctr++;
+        };
+        displayArr.push(
+            <div key={i} className={cls.Main_Container}>
+                <object key={i+'gg'}
+                    className={cls.Main_Container_Cogs_Left} 
+                    type="image/svg+xml" 
+                    data={Tryby}>
+                    svg-animation
+                </object>
+                <ol key={i+'xx'}>
+                    {displayElement}
+                </ol>
+                <object 
+                    key={i+'yy'}
+                    className={cls.Main_Container_Cogs_Right}
+                    type="image/svg+xml" 
+                    data={Tryby}>
+                    svg-animation
+                </object>
+            </div>
+        );
+    };
+
     return (
-        <h2>Projekty</h2>
+        <div className={`${cls.Main}`}>
+            <h2>{lctx.x.mainMenu[lang].projects}</h2>
+            {displayArr}
+        </div>
     );
 }
 
