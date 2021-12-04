af098d8067355e7be251c9cc8370d0c05b6f9db0
Maciej Uberna <maciej.uberna@gmail.com>
2021-12-04 14:14:08 +0100

    Done "Edukacja" tab.

--- portfolio/src/coponents-stateLess/Content/Edukacja/Edukacja.js
+++ portfolio/src/coponents-stateLess/Content/Edukacja/Edukacja.js
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import languageContext from '../../../mainStates/language/language-context';
+import GrCap from '../../../assets/images/Education/graduation-cap.svg';
+import cls from './Edukacja.module.scss';
 
 const Edukacja = (props) => {
+    const lctx = useContext(languageContext);
+    const [lang,setLang] = useState(lctx.x.language);
+
+    useEffect(() => {
+        setLang(lctx.x.language);
+    },[lctx.x.language]);
+
+    const eduArr = lctx.x.education[lang];
+    const displayArr = [];
+    for(let i=0;i<eduArr.length;i++){
+        const displayElement = [];
+        let ctr = 0;
+        for(let key in eduArr[i]) {
+            if (ctr===6) {
+                displayElement.push(
+                    <li
+                        key={i+key}
+                        className={cls.Main_Container_Diploma}
+                    >
+                        <p className={cls.Main_Container_Diploma_Title}>
+                            {key}
+                        </p>
+                        <img 
+                        src={eduArr[i][key]}
+                        />
+                    </li>
+                );
+            } else {
+                displayElement.push(
+                    <li
+                        key={i+key}
+                        
+                    >
+                        <img src={GrCap} className={cls.Main_Container_GrCap} /> 
+                        <span className={cls.Main_Container_Name}> {key} :</span> 
+                        <span className={cls.Main_Container_Desc}> {eduArr[i][key]} </span>
+                    </li>
+                )
+            }
+            ctr++;
+        };
+        displayArr.push(
+            <ul key={i} className={cls.Main_Container_ul}>
+                {displayElement}
+            </ul>
+        );
+    };
+
     return (
-        <h2>Edukacja</h2>
+        <div className={`${cls.Main}`}>
+            <h2>{lctx.x.mainMenu[lang].education}</h2>
+            <div className={cls.Main_Container}>
+                {displayArr}
+            </div>
+        </div>
     );
 }
 
