69f015d7ddb4fd7838bba3c2911c663825fe1791
Maciej Uberna <maciej.uberna@gmail.com>
2021-12-05 16:41:31 +0100

    Finished Doswiadczenie.js and fixed resolution error in variables.scss.

--- portfolio/src/coponents-stateLess/Content/Doswiadczenie/Doswiadczenie.js
+++ portfolio/src/coponents-stateLess/Content/Doswiadczenie/Doswiadczenie.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import languageContext from '../../../mainStates/language/language-context';
+import cls from './Doswiadczenie.module.scss';
 
-const Doswiadczenie = (props) => {
+const Doswiadczenie = () => {
+    const lctx = useContext(languageContext);
+    const [lang,setLang] = useState(lctx.x.language);
+
+    const expArr = lctx.x.job_exp[lang];
+    const displayArr = [];
+    for(let i=0;i<expArr.length;i++){
+        displayArr.push(
+            <li key={i+'lista'}>
+                <p>
+                    <span className={cls.Main_Container_JobPosition}>
+                        {expArr[i]["job-position"]}
+                    </span>
+                    <span className={cls.Main_Container_Interval}> 
+                        &nbsp;/ {expArr[i]["start-date"]} - {expArr[i]["end-date"]}
+                    </span>
+                </p>
+                <p className={cls.Main_Container_Company}>
+                    <span>
+                        {expArr[i]["company"]} ({expArr[i]["localization"]})
+                    </span>
+                </p>
+                <p className={cls.Main_Container_Desc}>
+                    <span>
+                        {expArr[i]["job-desc"]}
+                    </span>
+                </p>
+                <p className={cls.Main_Container_Git}>
+                    <span >
+                        {expArr[i]["git"]}
+                    </span>
+                </p>
+            </li>
+        );
+    }   
+
+    useEffect(() => {
+        setLang(lctx.x.language);
+    },[lctx.x.language]);
+
     return (
-        <h2>Doswiadczenie</h2>
+        <div className={cls.Main}>
+        <h2>{lctx.x.mainMenu[lang].job_exp}</h2>
+        <ol className={cls.Main_Container}>
+            {displayArr}
+        </ol>
+        </div>
     );
 }
 
