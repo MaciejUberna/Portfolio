c69edb41b244f7c31e52ef0326326f925253bd31
Maciej Uberna <maciej.uberna@gmail.com>
2022-12-18 15:33:40 +0100

    Projects updated, gallery not included yet.

--- portfolio/src/assets/texts/Projekty/projects_EN.js
+++ portfolio/src/assets/texts/Projekty/projects_EN.js
@@ -1,48 +1,30 @@
 export default [{
-        'Project name': 'Mój Burger',
+        'Project name': 'My Burger',
         'Technologies used': 'React, javascript, css',
         'Describtion': 'Fictional made in polish language burger selling app, that allows to choose your own burger recipe. On page Rejestracja/Logowanie your account creation is set as default, if you already have an account please click "Zmień na Zalogój"',
         'Test account': 'login: tmp100tmp@sharklasers.com password: Burger2021#. You can change the password via "https://www.guerrillamail.com/" service.',
         'Link': 'https://maciej-my-burger.web.app',
-        'GithubSSH': 'git@github.com:MaciejUberna/BurgerWWWapp.git',
-        'GithubHTML': 'https://github.com/MaciejUberna/BurgerWWWapp.git',
-        'GithubCLI': 'gh repo clone MaciejUberna/BurgerWWWapp'
+        'Github': 'https://github.com/MaciejUberna/BurgerWWWapp.git',
     },
     {
         'Project name': 'Portfolio',
         'Technologies used': 'React, javascript, sass',
         'Describtion': 'Web-page that prasents my biggest projects. Written in 3 languages: Polish, English and Spanish',
-        'Test account': 'not applicable',
-        'Link': '-------',
-        'GithubSSH': 'git@github.com:MaciejUberna/Portfolio.git',
-        'GithubHTML': 'https://github.com/MaciejUberna/Portfolio.git',
-        'GithubCLI': 'gh repo clone MaciejUberna/Portfolio'
+        'Github': 'https://github.com/MaciejUberna/Portfolio.git',
     },{
         'Project name': 'Naturomaniacy',
         'Technologies used': 'sass',
         'Describtion': 'Css template made in sass for fictional sidesing&travel agency: "Naturomaniacy". This exercise consists of css animations and filters.',
-        'Test account': 'not applicable',
-        'Link': '-------',
-        'GithubSSH': 'git@github.com:MaciejUberna/Naturomaniacy.git',
-        'GithubHTML': 'https://github.com/MaciejUberna/Naturomaniacy.git',
-        'GithubCLI': 'gh repo clone MaciejUberna/Naturomaniacy'
+        'Github': 'https://github.com/MaciejUberna/Naturomaniacy.git',
     },{
         'Project name': 'Trillo',
         'Technologies used': 'sass',
         'Describtion': 'Css template made for fictional hotel booking company. Exercise consists of flex-box praktice.',
-        'Test account': 'not applicable',
-        'Link': '-------',
-        'GithubSSH': 'git@github.com:MaciejUberna/Trillo.git',
-        'GithubHTML': 'https://github.com/MaciejUberna/Trillo.git',
-        'GithubCLI': 'gh repo clone MaciejUberna/Trillo'       
+        'Github': 'https://github.com/MaciejUberna/Trillo.git',      
     },{
         'Project name': 'Nexter',
         'Technologies used': 'sass',
         'Describtion': 'Css template made in sass for fictional company that sells luxory homes. Exercise consists of css-grid praktice.',
-        'Test account': 'not applicable',
-        'Link': '-------',
-        'GithubSSH': 'git@github.com:MaciejUberna/Nexter.git',
-        'GithubHTML': 'https://github.com/MaciejUberna/Nexter.git',
-        'GithubCLI': 'gh repo clone MaciejUberna/Nexter'         
+        'Github': 'https://github.com/MaciejUberna/Nexter.git',        
     }
 ];
